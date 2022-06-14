9b5d532be0bef81f342b16470725cf317dfba10d
huynhbaophuc179 <huynhbaophuc179a@gmail.com>
2022-06-14 13:41:23 +0700

    NPM script Part 1

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    $('#reserveModal').on('shown.bs.modal', function () {
+        $(document).off('focusin.modal');
+    });
+
+
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+
+
+    $("#reserveButton").click(function () {
+        $('#reserveModal').modal('show')
+    });
+
+    $("#loginButton").click(function () {
+        $('#loginModal').modal('show')
+    });
+})
